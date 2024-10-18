bf818f105dc9d55894cd802a247a5ac55014022a
Fatih Kürekçi <fatihkr02@gmail.com>
2024-10-19 01:22:50 +0300

    completed 'cari' processes in admin side

---
+++ Controllers/CariController.cs
@@ -0,0 +1,73 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class CariController : Controller
+    {
+        Context context = new Context();
+
+        public ActionResult Index()
+        {
+            var cariler = context.Caris.Where(x => x.Durum == true).ToList();
+            return View(cariler);
+        }
+
+        [HttpGet]
+        public ActionResult CariEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CariEkle(Cari cari)
+        {
+            context.Caris.Add(cari);
+            cari.Durum = true;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult CariSil(int id)
+        {
+            var cari = context.Caris.Find(id);
+            cari.Durum = false;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult CariGetir(int id)
+        {
+            var cari = context.Caris.Find(id);
+            return View("CariGetir", cari);
+        }
+
+        public ActionResult CariGuncelle(Cari cari)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("CariGetir");
+            }
+            var existCari = context.Caris.Find(cari.CariId);
+            existCari.CariAd = cari.CariAd;
+            existCari.CariSoyad = cari.CariSoyad;
+            existCari.CariSehir = cari.CariSehir;
+            existCari.CariMail = cari.CariMail;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult MusteriSatis(int id)
+        {
+            var values = context.SatisHarekets.Where(x => x.CariId == id).ToList();
+            var cari = context.Caris.Where(x => x.CariId == id).Select(y => y.CariAd + " " + y.CariSoyad).FirstOrDefault();
+            ViewBag.Cari = cari;
+
+            return View(values);
+        }
+    }
+}
--- Models/Entities/Cari.cs
+++ Models/Entities/Cari.cs
@@ -13,11 +13,12 @@ namespace MvcOnlineTicariOtomasyon.Models.Entities
         public int CariId { get; set; }
 
         [Column(TypeName = "Varchar")]
-        [StringLength(30)]
+        [StringLength(30, ErrorMessage = "En fazla 30 karakter yazabilirsiniz!")]
         public string CariAd { get; set; }
 
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
+        [Required(ErrorMessage = "Bu alanı boş geçemezsiniz!")]
         public string CariSoyad { get; set; }
 
         [Column(TypeName = "Varchar")]
@@ -27,6 +28,7 @@ namespace MvcOnlineTicariOtomasyon.Models.Entities
         [Column(TypeName = "Varchar")]
         [StringLength(50)]
         public string CariMail { get; set; }
+        public bool Durum { get; set; }
         public ICollection<SatisHareket> SatisHarekets { get; set; }
     }
 }
