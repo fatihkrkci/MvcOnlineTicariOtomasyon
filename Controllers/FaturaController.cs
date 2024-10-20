59d4780c77a14023bbf1e7a3ef0da3acde2294ad
Fatih Kürekçi <fatihkr02@gmail.com>
2024-10-20 23:58:18 +0300

    added fatura controller and created index page

---
+++ Controllers/FaturaController.cs
@@ -0,0 +1,20 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class FaturaController : Controller
+    {
+        Context context = new Context();
+
+        public ActionResult Index()
+        {
+            var liste = context.Faturas.ToList();
+            return View(liste);
+        }
+    }
+}
