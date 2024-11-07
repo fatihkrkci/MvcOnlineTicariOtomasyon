fc5b04c921a32a19f716714382516d518d1c223b
Fatih Kürekçi <fatihkr02@gmail.com>
2024-11-07 13:59:40 +0300

    added e-commerce theme and product detail page

---
+++ Controllers/UrunDetayController.cs
@@ -0,0 +1,23 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class UrunDetayController : Controller
+    {
+        Context context = new Context();
+
+        public ActionResult Index()
+        {
+            Class1 cs = new Class1();
+            cs.Deger1 = context.Uruns.Where(x => x.UrunId == 1).ToList();
+            cs.Deger2 = context.Detays.Where(y => y.DetayId == 1).ToList();
+
+            return View(cs);
+        }
+    }
+}
