65fff34a0c828265ce0d1b0d3bac42b621c0728a
Fatih Kürekçi <fatihkr02@gmail.com>
2024-11-24 15:10:01 +0300

    Dynamic invoice transactions completed

--- Controllers/FaturaController.cs
+++ Controllers/FaturaController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Schema;
 
 namespace MvcOnlineTicariOtomasyon.Controllers
 {
@@ -75,6 +76,43 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             context.SaveChanges();
 
             return RedirectToAction("Index");
+        }
+
+        public ActionResult Dinamik()
+        {
+            Class4 cs = new Class4();
+            cs.Deger1 = context.Faturas.ToList();
+            cs.Deger2 = context.FaturaKalems.ToList();
+            return View(cs);
+        }
+
+        public ActionResult FaturaKaydet(string FaturaSeriNo, string FaturaSiraNo, DateTime Tarih, string VergiDairesi, string Saat, string TeslimEden, string TeslimAlan, string Toplam, FaturaKalem[] kalemler)
+        {
+            Fatura f = new Fatura();
+            f.FaturaSeriNo = FaturaSeriNo;
+            f.FaturaSiraNo = FaturaSiraNo;
+            f.Tarih = Tarih;
+            f.VergiDairesi = VergiDairesi;
+            f.Saat = Saat;
+            f.TeslimEden = TeslimEden;
+            f.TeslimAlan = TeslimAlan;
+            f.Toplam = decimal.Parse(Toplam);
+
+            context.Faturas.Add(f);
+
+            foreach (var item in kalemler)
+            {
+                FaturaKalem fk = new FaturaKalem();
+                fk.Aciklama = item.Aciklama;
+                fk.BirimFiyat = item.BirimFiyat;
+                fk.FaturaId = item.FaturaKalemId;
+                fk.Miktar = item.Miktar;
+                fk.Tutar = item.Tutar;
+                context.FaturaKalems.Add(fk);
+            }
+
+            context.SaveChanges();
+            return Json("İşlem Başarılı", JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ Models/Entities/Class4.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Models.Entities
+{
+    public class Class4
+    {
+        public IEnumerable<Fatura> Deger1 { get; set; }
+        public IEnumerable<FaturaKalem> Deger2 { get; set; }
+    }
+}
